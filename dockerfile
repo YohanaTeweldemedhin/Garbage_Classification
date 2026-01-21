1585d78082d2f6303ae0a42d7d973ac2f6fd6774
YohanaTeweldemedhin <hanatkle@gmail.com>
2026-01-21 23:19:16 +0100

    update readme

--- dockerfile
+++ dockerfile
@@ -1,22 +1,26 @@
-FROM public.ecr.aws/lambda/python:3.8
+# AWS Lambda Python 3.11 base image
+FROM public.ecr.aws/lambda/python:3.11
 
-RUN pip install -r requirements.txt
-RUN pip install keras-image-helper
-RUN pip install --etra-index-url \ 
-https://google-coral.github.io/py-repo/ tflite_runtime
-COPY xception_v4_1_08_0.940.tflite .
+# Set working directory (Lambda uses /var/task)
+WORKDIR /var/task
+
+# Install system dependencies needed for TensorFlow Lite + image processing
+RUN yum install -y \
+    mesa-libGL \
+    glib2 \
+    libSM \
+    libXext \
+    libXrender \
+    && yum clean all
+
+# Copy requirements and install Python dependencies into Lambda task root
+COPY requirements.txt .
+RUN pip install --upgrade pip \
+    && pip install --no-cache-dir -r requirements.txt --target .
+
+# Copy your Lambda function and model
 COPY lambda_function.py .
+COPY xception_v4_1_08_0.940.tflite .
+
+# Lambda handler: file.function_name
 CMD ["lambda_function.lambda_handler"]
-
-
-
-FROM python:3.8-slim
-
-RUN pip install --upgrade pip
-
-RUN pip install keras-image-helper
-
-RUN pip install --extra-index-url \
-    https://google-coral.github.io/py-repo/ tflite_runtime
-
-
--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,14 @@
-import tensorflow.lite as tflite
+
+import tensorflow as tf
 from keras_image_helper import create_preprocessor
+import os
 
 
+classes = ['battery', 'cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']  # Update as per your dataset
 preprocessor = create_preprocessor('xception', target_size=(299, 299))
-interpreter = tflite.Interpreter(model_path='xception_v4_1_08_0.940.tflite')
+
+# Load TFLite model
+interpreter = tf.lite.Interpreter(model_path='xception_v4_1_08_0.940.tflite')
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
@@ -12,25 +17,21 @@ output_details = interpreter.get_output_details()
 input_index = input_details[0]['index']
 output_index = output_details[0]['index']
 
-
-# path = r"D:/GitHub/Garbage_Classification/dataset/test/battery/battery_4.jpg"
-
-
 def predict(path):
+    """Predict class probabilities for an image"""
     x = preprocessor.from_path(path)
-
     interpreter.set_tensor(input_index, x)
     interpreter.invoke()
     preds = interpreter.get_tensor(output_index)
-
     return dict(zip(classes, preds[0]))
 
-
-def lambda_handler(event, contet):
-    url= event['url']
-
-    result= predict (url)
+def lambda_handler(event, context):
+    """Simulate AWS Lambda handler"""
+    url = event['url']
+    result = predict(url)
     return result
 
-
-
+if __name__ == "__main__":
+    # For local testing
+    test_path = "dataset/test/battery/battery_4.jpg"
+    print(predict(test_path))
